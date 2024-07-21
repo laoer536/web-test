5a74955ae78c9af476a52f87b9629f908491081e
LiuJie <1547698569@qq.com>
2024-07-21 17:10:42 +0800

    build: playwright for test

--- README.md
+++
@@ -1,2 +0,0 @@
-# web-test
-for web test. 
--- axios/axios-setup.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosRequestConfig } from 'axios'
-
-/** 定义axios基础配置 */
-export const axiosBaseOptions: AxiosRequestConfig = {
-  baseURL: '',
-  timeout: 8000,
-}
